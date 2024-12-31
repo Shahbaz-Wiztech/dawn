c9492e1661cb8414eb593e59913d9ef29a21e140
Shahbaz-Wiztech <132899070+Shahbaz-Wiztech@users.noreply.github.com>
2024-12-31 18:40:10 +0500

    product details page

--- assets/application.js
+++ assets/application.js
@@ -1,42 +1,20 @@
     console.log('first')
+    // product details page image zoom effect
+    function zoom(e) {
+      var zoomer = e.currentTarget;
+      e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+      e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+      x = offsetX / zoomer.offsetWidth * 100
+      y = offsetY / zoomer.offsetHeight * 100
+      zoomer.style.backgroundPosition = x + '% ' + y + '%';
+    }
     // bootstrap tooltip
-    // document.addEventListener('DOMContentLoaded', function () {
-    //     const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    //     const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    //       return new bootstrap.Tooltip(tooltipTriggerEl);
-    //     });
-    //   });
-  
-  
-      // image modal
-      const modal = document.getElementById('myModal');
-      const closeModal = document.querySelector('.close');
-      const carouselInner = document.getElementById('carouselInner');
-      const images = document.querySelectorAll('.modal-trigger');
-      images.forEach((img, index) => {
-        img.onclick = function () {
-            console.log('inside the function');
-          modal.style.display = "block";
-        //   carouselInner.innerHTML = "";
-        //   images.forEach((carouselImg, carouselIndex) => {
-        //     const carouselItem = document.createElement('div');
-        //     carouselItem.className = `carousel-item ${carouselIndex === index ? 'active' : ''}`;
-        //     carouselItem.innerHTML = `
-        //      <img src="${carouselImg.src}" class="d-block modal-content" alt="${carouselImg.alt}">`;
-        //     carouselInner.appendChild(carouselItem);
-        //   });
-        };
+    document.addEventListener('DOMContentLoaded', function () {
+        const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+          return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
       });
-      closeModal.onclick = function () {
-        modal.style.display = "none";
-      };
-  
-      window.onclick = function (event) {
-        if (event.target === modal) {
-          modal.style.display = "none";
-        }
-      };
-// ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //     // price range slider
@@ -126,3 +104,29 @@ function gridListToggle(event){
         productContainer.classList.add('product-list');
     }
 }
+
+// function currentTarget(event , activeClass){
+//   console.log('inside function')
+//   const currentElement = event.currentTarget;
+//   if(currentElement.classList.contains(activeClass)){
+//     currentElement.classList.remove(activeClass);
+//   }else{
+//     currentElement.classList.add(activeClass);
+//   }
+// }
+
+  // Add to cart Quantity js;
+  function incrementQuantity(button){
+    const currentButton = button.currentTarget;
+    const quantityInput = currentButton.closest("form").querySelector(".quantity");
+    const currentValue = parseInt(quantityInput.value, 10);
+    quantityInput.value = currentValue + 1;
+  }
+  function decrementQuantity(button){
+    const currentButton = button.currentTarget;
+    const quantityInput = currentButton.closest("form").querySelector(".quantity");
+    const currentValue = parseInt(quantityInput.value, 10);
+    if (currentValue > 1) {
+      quantityInput.value = currentValue - 1;
+    }
+  }
